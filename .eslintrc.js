d1ff3ce190f4ca2673c96421cd63126e41c60ebc
Oli Griffiths <oligriffiths@users.noreply.github.com>
2018-07-23 11:37:06 +0100

    Hello Broccoli

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  plugins: ['node', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  env: {
+    node: true,
+  },
+  rules: {
+    strict: 'error',
+    'no-var': 'error',
+    'no-console': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'error',
+    'prettier/prettier': ['error', require('./prettier.config')],
+  }
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# Broccolijs.com Website
+
+Welcome to the Broccoli.js website. This repository contains the source files for the
+website, including a build pipeline to generate a new static version of the site.
