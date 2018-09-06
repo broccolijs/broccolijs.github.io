24d16080d9b06b2ebd2bba28a155b9aba6b32adb
Oli Griffiths <oligriffiths@users.noreply.github.com>
2018-09-06 10:21:42 -0400

    Add styles

--- Brocfile.js
+++ Brocfile.js
@@ -1,9 +1,21 @@
 'use strict';
 
 const md = require('./lib/plugin/markdown-handlebars');
+const merge = require('broccoli-merge-trees');
+const sass = require('broccoli-sass-source-maps')(require('sass'));
 
-module.exports = new md('docs', {
+const markdown = new md('docs', {
   layouts: {
     default: 'src/templates/index.hbs',
   },
 });
+
+const css = new sass(['src'], 'styles/site.scss', 'assets/site.css', {
+  annotation: 'Sass files',
+  sourceMap: true,
+  sourceMapContents: true,
+});
+
+const tree = merge([markdown, css]);
+
+module.exports = tree;
