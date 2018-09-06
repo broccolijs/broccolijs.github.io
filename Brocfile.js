1dac980bc8cf06d418576512722eb3f5aaf1c4c8
Oli Griffiths <oligriffiths@users.noreply.github.com>
2018-09-06 10:45:41 -0400

    Add logo

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,7 @@
 const md = require('./lib/plugin/markdown-handlebars');
 const merge = require('broccoli-merge-trees');
 const sass = require('broccoli-sass-source-maps')(require('sass'));
+const funnel = require('broccoli-funnel');
 
 const markdown = new md('docs', {
   layouts: {
@@ -10,12 +11,16 @@ const markdown = new md('docs', {
   },
 });
 
-const css = new sass(['src'], 'styles/site.scss', 'assets/site.css', {
+const css = sass(['src'], 'styles/site.scss', 'assets/site.css', {
   annotation: 'Sass files',
   sourceMap: true,
   sourceMapContents: true,
 });
 
-const tree = merge([markdown, css]);
+const pub = funnel('src/public', {
+  annotation: 'Public assets',
+});
+
+const tree = merge([markdown, css, pub]);
 
 module.exports = tree;
