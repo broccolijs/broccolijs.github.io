f9facefbe2a060c0059a0f5f48e351e0d0569e0f
Oli Griffiths <oligriffiths@users.noreply.github.com>
2018-11-29 18:44:18 -0500

    Asset rev

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,10 @@ const md = require('./lib/plugin/markdown-handlebars');
 const merge = require('broccoli-merge-trees');
 const sass = require('broccoli-sass-source-maps')(require('sass'));
 const funnel = require('broccoli-funnel');
+const assetRev = require('broccoli-asset-rev');
 const LiveReload = require('broccoli-livereload');
+const env = require('broccoli-env').getEnv() || 'development';
+const isProduction = env === 'production';
 
 const markdown = new md('src/docs', 'src/templates');
 
@@ -20,8 +23,14 @@ const pub = funnel('src/public', {
 
 let tree = merge([markdown, css, pub]);
 
-tree = new LiveReload(tree, {
-  target: 'index.html',
-});
+if (isProduction) {
+  tree = assetRev(tree, {
+    extensions: ['js', 'css', 'png', 'jpg', 'gif', 'webmanifest', 'svg'],
+  });
+} else {
+  tree = new LiveReload(tree, {
+    target: 'index.html',
+  });
+}
 
 module.exports = tree;
