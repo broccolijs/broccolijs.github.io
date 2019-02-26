081890e0d02984c15368afbc9b37ddc99f937ae2
Oli Griffiths <oligriffiths@users.noreply.github.com>
2019-02-25 21:08:54 -0500

    Use latest Broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,55 +1,59 @@
 'use strict';
 
-const md = require('./lib/plugin/markdown-handlebars');
-const merge = require('broccoli-merge-trees');
-const sass = require('broccoli-sass-source-maps')(require('sass'));
-const sassLint = require('broccoli-sass-lint');
-const funnel = require('broccoli-funnel');
-const assetRev = require('broccoli-asset-rev');
-const LiveReload = require('broccoli-livereload');
-const CleanCss = require('broccoli-clean-css');
-const env = require('broccoli-env').getEnv() || 'development';
-const isProduction = env === 'production';
+import md from './lib/plugin/markdown-handlebars';
+import merge from 'broccoli-merge-trees';
+import broccoliSass from 'broccoli-sass-source-maps';
+import sass from 'sass';
+import sassLint from 'broccoli-sass-lint';
+import funnel from 'broccoli-funnel';
+import assetRev from 'broccoli-asset-rev';
+import LiveReload from 'broccoli-livereload';
+import CleanCss from 'broccoli-clean-css';
 
-const appRoot = 'src';
+const compileSass = broccoliSass(sass);
 
-const markdown = new md(`${appRoot}/content`, `${appRoot}/templates`, {
-  data: {
-    title: 'broccoli.build',
-    description: 'Broccoli.js - The asset pipeline for ambitious web applications',
-    url: 'https://broccoli.build',
-    menu: require('./menu'),
-  },
-});
+export default (options) => {
+  const appRoot = 'src';
+  const isProduction = options.env === 'production';
 
-let css = sassLint(appRoot + '/styles', {
-  disableTestGenerator: true,
-});
-
-css = sass([css, 'node_modules'], 'site.scss', 'assets/site.css', {
-  annotation: 'Sass files',
-  sourceMap: true,
-  sourceMapContents: true,
-});
-
-if (isProduction) {
-  css = new CleanCss(css);
-}
-
-const pub = funnel('src/public', {
-  annotation: 'Public assets',
-});
-
-let tree = merge([markdown, css, pub]);
-
-if (isProduction) {
-  tree = assetRev(tree, {
-    extensions: ['js', 'css', 'png', 'jpg', 'gif', 'webmanifest', 'svg'],
+  const markdown = new md(`${appRoot}/content`, `${appRoot}/templates`, {
+    data: {
+      title: 'broccoli.build',
+      description: 'Broccoli.js - The asset pipeline for ambitious web applications',
+      url: 'https://broccoli.build',
+      menu: require('./menu'),
+    },
   });
-} else {
-  tree = new LiveReload(tree, {
-    target: '\\.html$',
-  });
-}
 
-module.exports = tree;
+  let css = sassLint(appRoot + '/styles', {
+    disableTestGenerator: true,
+  });
+
+  css = compileSass([css, 'node_modules'], 'site.scss', 'assets/site.css', {
+    annotation: 'Sass files',
+    sourceMap: true,
+    sourceMapContents: true,
+  });
+
+  if (isProduction) {
+    css = new CleanCss(css);
+  }
+
+  const pub = funnel('src/public', {
+    annotation: 'Public assets',
+  });
+
+  let tree = merge([markdown, css, pub]);
+
+  if (isProduction) {
+    tree = assetRev(tree, {
+      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'webmanifest', 'svg'],
+    });
+  } else {
+    tree = new LiveReload(tree, {
+      target: '\\.html$',
+    });
+  }
+
+  return tree;
+}
